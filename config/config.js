cbafb73a1d77aca66fc6d82c4b15455a2e22b03f
yolovi <yolandalopevi@gmail.com>
2023-07-13 17:14:58 +0200

    created keys.example.js and installed dependencies

--- config/config.js
+++ config/config.js
@@ -6,10 +6,10 @@ const { MONGO_URI } = require("./keys");
 const dbConnection = async () => {
     try {
         await mongoose.connect(MONGO_URI);
-        console.log("Base de datos conectada con éxito");
+        console.log("Database connected succesfully");
     } catch (error) {
         console.error(error);
-        throw new Error("Error a la hora de iniciar la base de datos");
+        throw new Error("Error connecting to database");
     }
 };
 module.exports = {
---
+++ config/keys.example.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'Paste your connection string here' 
+    }
