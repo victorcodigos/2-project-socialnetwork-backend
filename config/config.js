ba4527ed45cee358811aff9b7def6b247509df13
yolovi <yolandalopevi@gmail.com>
2023-07-15 21:45:59 +0200

    solved conflict with config and saved changes tokens user array victor

--- config/config.js
+++ config/config.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("./keys");
 
-
-
 const dbConnection = async () => {
-    const MONGO_URI = `mongodb+srv://victormacedodedeus1:y3ul7ehLjcnyabys@cluster0.yr2umvr.mongodb.net/?retryWrites=true&w=majority`;
     try {
         await mongoose.connect(MONGO_URI);
         console.log("Database connected succesfully");
@@ -13,7 +10,9 @@ const dbConnection = async () => {
         throw new Error("Error connecting to database");
     }
 };
+
 module.exports = {
     dbConnection,
-
 };
+
+
