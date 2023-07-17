be4fdcc6cd1c67522cb27b91198e66594c87e02e
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-07-17 12:51:17 +0200

    PostController has been created!

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -9,6 +9,17 @@ const PostController = {
             console.error(error);
             res.status(500).send({message: "Try again! something is not working well", error})
         }
+    },
+
+    //Endpoint para actualizar un post ( tiene que estar autenticado) 
+
+    async update(req, res){
+        try {
+            const post = await Post.findByIdAndUpdate(req.params._id, req.body, { new: true })
+            res.send({message:"This post has been updated successfully", post});
+        } catch (error) {
+            console.error({message: "Sorry! We could not updated this post! You need to be authorized!", error})
+        }
     }
 }
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const PostController = require('../controllers/PostController');
+const { authentication } = require('../middleware/authentication');
 const router = express.Router()
 
 
 router.post('/',PostController.create)
+router.put('/id/:_id', authentication, PostController.update)
 
 
 module.exports = router;
