a9a09c7cff382db941da7d01acde3d2b0cc30720
yolovi <yolandalopevi@gmail.com>
2023-07-17 15:31:12 +0200

    update comment endpoint

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -6,12 +6,43 @@ const CommentController ={
     async create(req, res){
         try {
             const comment = await Comment.create(req.body)
-            res.status(201).send(comment)
+            res.status(201).send({message: "Comment posted", comment})
         } catch (error) {
             console.error(error)
             res.status(500).send({ message: 'Error posting the comment' })
         }
     },
+    async update(req, res){
+        try {
+            const comment = await Comment.findByIdAndUpdate(req.params._id, req.body, { new: true })
+            res.send({message:"This comment has been updated successfully", comment});
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({ message: "Sorry! We could not updated this comment! You need to be authorized!", error })
+        }
+    },
 
 }
+
+
 module.exports = CommentController;
+
+
+// const OrderController = {
+//     . . .
+//      async update(req, res) {
+//       try {
+//         const order = await Order.findByIdAndUpdate(
+//           req.params._id,
+//           { ...req.body, userId: req.user._id },
+//           {
+//             new: true,
+//           }
+//         );
+//         res.send({ message: "order successfully updated", order });
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     },
+//   }
+  
--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,7 @@ const CommentController = require('../controllers/CommentController');
 const router = express.Router()
 
 
-router.post('/', CommentController.create)
+router.post('/', CommentController.create);
+router.put('/id/:_id', CommentController.update)
 
 module.exports = router;
