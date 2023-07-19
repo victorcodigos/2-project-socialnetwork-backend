ab7990dccd32ebff25723e8e50fcdf01124305c0
yolovi <yolandalopevi@gmail.com>
2023-07-19 11:26:17 +0200

    demoUsers changed password and nodemailer.example.js created

---
+++ config/nodemailer.example.js
@@ -0,0 +1,12 @@
+const nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'your gmail email',
+        pass: 'goolge password generated'
+    }
+});
+
+module.exports = {transporter}; 
